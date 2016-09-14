c6f4b7595db5ce162c247085329a4c83cd4f43ad
itay.gliksman <itay.gliksman@overwolf.com>
2016-09-14 13:36:16 +0300

    dota-events-sample-app

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', function () {
 
-  // this a subset of the features that DOTA events provides - however,
+  // this a subset of the features that LoL events provides - however,
   // when writing an app that consumes events - it is best if you request
   // only those features that you want to handle.
   //
-  // NOTE: in the future we'll have a wildcard option to allow retrieving all
+  // NOTE: in the future we'll have a wildcard option to allow retreiving all
   // features
   var textarea = document.getElementById('textareaMessage');
 
-  var g_interestedInFeatures = [
+  var requestedFeatures = [
 
     'kill',
     'death',
@@ -60,7 +60,7 @@ window.addEventListener('load', function () {
     // 'static' data changed (total kills, username, steam-id)
     // This will also be triggered the first time we register
     // for events and will contain all the current information
-    overwolf.games.events.onInfoUpdates.addListener(function(info) {
+    overwolf.games.events.onInfoUpdates2.addListener(function(info) {
       var log = 'Info UPDATE: ' + JSON.stringify(info);
       textarea.value += log + '\n';
       console.log(log);
@@ -74,23 +74,87 @@ window.addEventListener('load', function () {
     });
   }
 
-  function setFeatures() {
 
-    overwolf.games.events.setRequiredFeatures(g_interestedInFeatures, function(info) {
-      console.log('Set required features:');
-      console.log(JSON.stringify(info));
-      if (info.status == 'error') {
-        //console.log('Could not set required features: ' + info.reason);
-        //console.log('Trying in 2 seconds');
-        window.setTimeout(setFeatures, 2000);
-        return;
-      }
 
+    function gameLaunched(gameInfoResult) {
+        if (!gameInfoResult) {
+            return false;
+        }
+
+        if (!gameInfoResult.gameInfo) {
+            return false;
+        }
+
+        if (!gameInfoResult.runningChanged && !gameInfoResult.gameChanged) {
+            return false;
+        }
+
+        if (!gameInfoResult.gameInfo.isRunning) {
+            return false;
+        }
+
+        // NOTE: we divide by 10 to get the game class id without it's sequence number
+        if (Math.floor(gameInfo.id/10) != 7314) {
+            return false;
+        }
+
+        console.log("LoL Launched");
+        return true;
+
+    }
+
+    function gameRunning(gameInfo) {
+
+        if (!gameInfo) {
+            return false;
+        }
+
+        if (!gameInfo.isRunning) {
+            return false;
+        }
+
+        // NOTE: we divide by 10 to get the game class id without it's sequence number
+        if (Math.floor(gameInfo.id/10) != 7314) {
+            return false;
+        }
+
+        console.log("LoL running");
+        return true;
+
+    }
+
+
+    function setFeatures() {
+        overwolf.games.events.setRequiredFeatures(requestedFeatures, function(info) {
+            if (info.status == "error")
+            {
+                //console.log("Could not set required features: " + info.reason);
+                //console.log("Trying in 2 seconds");
+                window.setTimeout(setFeatures, 2000);
+                return;
+            }
+
+            console.log("Set required features:");
+            console.log(JSON.stringify(info));
+        });
+    }
+
+
+// Start here
+    overwolf.games.onGameInfoUpdated.addListener(function (res) {
+        console.log("onGameInfoUpdated: " + JSON.stringify(res));
+        if (gameLaunched(res)) {
+            registerEvents();
+            setTimeout(setFeatures, 1000);
+        }
     });
-  }
 
-  // Start here
-  console.log('DOTA GAME EVENTS CONSUMER SAMPLE APP');
-  registerEvents();
-  setTimeout(setFeatures, 3000);
+    overwolf.games.getRunningGameInfo(function (res) {
+        if (gameRunning(res)) {
+            registerEvents();
+            setTimeout(setFeatures, 1000);
+        }
+        console.log("getRunningGameInfo: " + JSON.stringify(res));
+    });
+
 });
